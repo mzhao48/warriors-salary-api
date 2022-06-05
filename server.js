abf7e5dc8931d5fa74df35d50306785f7bf7eb9c
Michael Zhao <michaelzhao48@gmail.com>
2022-06-05 10:35:13 -0700

    added PlayerInfo class

--- server.js
+++ server.js
@@ -5,29 +5,22 @@ const PORT = 8000
 
 app.use(cors())
 
-const players = {
-    'stephen curry' : {
-        'jersery number': 30,
-        'salary': 43006362,
-        'points per game': 32.0
-    },
-    'andrew wiggins' : {
-        'jersery number': 22,
-        'salary': 29542010,
-        'points per game': 18.6
-    },
-    'klay thompson' : {
-        'jersery number': 11,
-        'salary': 35361360,
-        'points per game': 0.0
-    },
-    'unknown' : {
-        'jersery number': 0,
-        'salary': 0,
-        'points per game': 0.0
+
+class PlayerInfo {
+    constructor(jerseyNumber, salary, pointsPerGame) {
+        this.jerseyNumber = jerseyNumber;
+        this.salary = salary;
+        this.pointsPerGame = pointsPerGame;
     }
 }
 
+const players = {
+    'stephen curry': new PlayerInfo(30,43006362,32.0),
+    'andrew wiggins': new PlayerInfo(22,29542010,18.6),
+    'klay thompson': new PlayerInfo(11,35361360,0.0),
+    'draymond green': new PlayerInfo(23,22246956,7.0),
+    'unknown': new PlayerInfo(0,0,0),
+}
 
 app.get('/', (request,response)=>{
     response.sendFile(__dirname + '/index.html')
